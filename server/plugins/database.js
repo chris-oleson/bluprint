ea7a45c2054ae57df6b15ab32cfa42f3c2058366
Chris Oleson <chris@oleson.family>
2025-02-28 12:10:41 -0700

    temp disable of database

--- server/plugins/database.js
+++ server/plugins/database.js
@@ -1,16 +1,16 @@
-import Mysql from 'mysql2/promise'
+// import Mysql from 'mysql2/promise'
 
-export default defineNitroPlugin(async (nitro) => {
-    const connectionPool = Mysql.createPool({
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: 'bluprint-db',
-        timezone: '+00:00'
-    })
+// export default defineNitroPlugin(async (nitro) => {
+//     const connectionPool = Mysql.createPool({
+//         host: process.env.DB_HOST,
+//         port: process.env.DB_PORT,
+//         user: process.env.DB_USER,
+//         password: process.env.DB_PASSWORD,
+//         database: 'bluprint-db',
+//         timezone: '+00:00'
+//     })
 
-    nitro.hooks.hook('request', (event) => {
-        event.context.database = connectionPool
-    })
-})
+//     nitro.hooks.hook('request', (event) => {
+//         event.context.database = connectionPool
+//     })
+// })
